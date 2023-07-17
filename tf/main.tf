a72546f81c311cf3d1fcf39f9c57887f0d84e92b
Nguyễn Ngói <nguyenngoi9190@gmail.com>
2023-07-17 16:29:22 +0700

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -13,10 +13,17 @@
 locals {
   region    = "us-west-2"
   name      = "test"
+  tags      ="tags_name"
   some_name = "some_test_name"
-  some      = "some_test_name"
+
+  s3_bucket_name      = "some_test_name"
 }
 
 provider "aws" {
   region = local.region
+}
+
+resource "aws_s3_bucket" "log_work_db" {
+  bucket = "${local.s3_bucket_name}"
+  tags = local.tags
 }
